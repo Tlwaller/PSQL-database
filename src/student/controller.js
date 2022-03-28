5648bd9d5f742f346543a9214735cee97fc878ca
Trey Waller <tlwaller99@gmail.com>
2022-03-28 14:59:33 -0500

    api is now restful

--- src/student/controller.js
+++ src/student/controller.js
@@ -1,12 +1,61 @@
 const pool = require("../../db");
+const queries = require("./queries");
 
 const getStudents = (req, res) => {
-  pool.query("SELECT * FROM students", (error, result) => {
+  pool.query(queries.getStudents, (error, results) => {
     if (error) throw error;
-    res.status(200).json(result.rows);
+    res.status(200).json(results.rows);
+  });
+};
+
+const getStudentById = (req, res) => {
+  const id = parseInt(req.params.id);
+  pool.query(queries.getStudentById, [id], (error, results) => {
+    if (error) throw error;
+    res.status(200).json(results.rows);
+  });
+};
+
+const addStudent = (req, res) => {
+  const { name, email, age, dob } = req.body;
+
+  //check if email exists
+  pool.query(queries.checkEmailExists, [email], (error, results) => {
+    if (results.rows.length) {
+      res.send("email already exists");
+    } else {
+      //add student to db
+      pool.query(
+        queries.addStudent,
+        [name, email, age, dob],
+        (error, results) => {
+          if (error) throw error;
+          res.status(201).send("student created successfully");
+        }
+      );
+    }
+  });
+};
+
+const deleteStudent = (req, res) => {
+  const id = parseInt(req.params.id);
+  pool.query(queries.getStudentById, [id], (error, results) => {
+    const noStudentFound = !results.rows.length;
+    if (error) throw error;
+    if (noStudentFound) {
+      res.send("student does not exist in the database");
+    } else {
+      pool.query(queries.deleteStudent, [id], (error, results) => {
+        if (error) throw error;
+        res.status(200).send("student successfully removed");
+      });
+    }
   });
 };
 
 module.exports = {
   getStudents,
+  getStudentById,
+  addStudent,
+  deleteStudent,
 };
--- src/student/routes.js
+++ src/student/routes.js
@@ -4,5 +4,8 @@ const controller = require("./controller");
 const router = Router();
 
 router.get("/", controller.getStudents);
+router.post("/", controller.addStudent);
+router.get("/:id", controller.getStudentById);
+router.delete("/:id", controller.deleteStudent);
 
 module.exports = router;
